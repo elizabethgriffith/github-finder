0656ff5a99e48dbfa779286e7de2022f6801bdbb
Elizabeth Griffith <elizabethgriffith97@gmail.com>
2022-09-09 14:11:08 -0400

    Create searchUser function

---
+++ app.js
@@ -0,0 +1,26 @@
+// Instantiate classes
+const github = new Github
+const ui = new UI
+
+// Event listener for user input
+const searchInput = document.getElementById('searchInput')
+searchInput.addEventListener('keyup', searchUser)
+
+function searchUser(e){
+  // Store user input
+  const userSearch = e.target.value
+  // Validate for empty input and make HTTP get request
+  if(userSearch !== ''){
+    github.getUser(userSearch)
+    // Validate for found user and display profile
+    .then(data => {
+      if(data.profile.message === 'Not Found'){
+        // show alert
+      } else {
+        // show profile
+      }
+    })
+  } else {
+    // Remove profile
+  }
+}
